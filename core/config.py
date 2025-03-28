31f4dca9ca8163e5f3fbe85c97dca14904a90ca3
shengnan <pelevin1111@gmail.com>
2025-03-28 17:21:04 +0300

    [UPD] add configs

--- core/config.py
+++ core/config.py
@@ -1,7 +1,19 @@
+import os
+
+from aiogram.types import BotCommand
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Settings:
-    pass
+    TOKEN = os.getenv("TOKEN")
+    PRAISES = ["лув киска, пока пуста, заполню как смогу"]
+    CAT_TAGS = ["cute", "funny", "sleepy", "play", "adorable"]
+    MENU_COMMANDS = [
+        BotCommand(command="/menu", description="Главное меню"),
+        BotCommand(command="/meow", description="Картинка с китиком"),
+        BotCommand(command="/praise", description="Похвалить"),
+        BotCommand(command="/send", description="Написать пете п.")
+    ]
+    ADMIN_ID = int(os.getenv("ADMIN_ID"))
